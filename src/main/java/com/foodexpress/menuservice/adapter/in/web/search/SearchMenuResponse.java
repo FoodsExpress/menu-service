6d1eeb1329a9e98fc13c42ca1ebd3a668085ecf8
cyeji <98408267+cyeji@users.noreply.github.com>
2024-03-19 21:26:53 +0900

    refactor : 사용하지 않는 변수 제거

--- src/main/java/com/foodexpress/menuservice/adapter/in/web/search/SearchMenuResponse.java
+++ src/main/java/com/foodexpress/menuservice/adapter/in/web/search/SearchMenuResponse.java
@@ -1,6 +1,5 @@
 package com.foodexpress.menuservice.adapter.in.web.search;
 
-import com.foodexpress.menuservice.domain.File;
 import com.foodexpress.menuservice.domain.Menu;
 import lombok.Getter;
 
@@ -10,7 +9,6 @@ import java.util.List;
 public class SearchMenuResponse {
 
     private List<Menu> menuList;
-    private List<File> menuImages;
 
     public static SearchMenuResponse of(List<Menu> menu) {
         SearchMenuResponse response = new SearchMenuResponse();
